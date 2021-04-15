3c3bead40f4f4390fcfad2428a8a5f8afc474ade
Callum Booth <callum.booth@launchb.co.uk>
2021-04-15 21:18:43 +0100

    remove sanity

--- sanity/resolveProductionUrl.js
+++
@@ -1,6 +0,0 @@
-const previewSecret = 'dsajlhflajhflh88fa8oUjJLHKfdasNM' // Copy the string you used for SANITY_PREVIEW_SECRET
-const projectUrl = 'http://localhost:3000'
-
-export default function resolveProductionUrl(document) {
-  return `${projectUrl}/api/preview?secret=${previewSecret}&slug=${document.slug.current}`
-}
