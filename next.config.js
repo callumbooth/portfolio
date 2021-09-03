dc01cad7d0f1f47995cecff5a78bca6315c7541c
Callum Booth <callum.booth@launchb.co.uk>
2021-09-03 22:05:29 +0100

    update next config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const withPlugins = require("next-compose-plugins");
 const withSentry = require("./utils/withSentry");
 
 module.exports = withPlugins([withSentry], {
-  future: {
-    webpack5: true,
-  },
   images: {
     domains: ["media.graphcms.com"],
   },
