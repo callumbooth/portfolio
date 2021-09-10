65ed34d13b80932d09d7d765e27b0d073ef40857
Callum Booth <callum.booth@launchb.co.uk>
2021-09-10 23:51:15 +0100

    updates code to use new generated query

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import { useRouter } from "next/router";
 import ErrorPage from "next/error";
-import {
-  getProjectBySlug,
-  useGetProjectBySlugQuery
-} from "@/root/hooks/useProjects";
+import { useGetProjectBySlug } from "@/queries";
 import RichText from "@/root/components/atoms/RichText";
+import { Stage } from "@/root/types/generated/schemas";
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
+import { ParsedUrlQuery } from "querystring";
 
-export default function Post({ title, body, preview }) {
+interface IPostProps {
+  preview: boolean;
+  title: string;
+  body: any;
+}
+
+interface IPostParams extends ParsedUrlQuery {
+  slug: string;
+}
+
+export default function Post({
+  title,
+  body,
+  preview
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   const router = useRouter();
 
   if (!router.isFallback && !title) {
@@ -28,8 +42,20 @@ export default function Post({ title, body, preview }) {
   );
 }
 
-export async function getStaticProps({ params, preview = false }) {
-  const data = await getProjectBySlug("super-theme", preview);
+export const getStaticProps: GetStaticProps<IPostProps, IPostParams> = async ({
+  params,
+  preview = false
+}) => {
+  const data = await useGetProjectBySlug.fetcher({
+    slug: params.slug,
+    draft: preview ? Stage.Draft : Stage.Published
+  })();
+
+  if (!data.project) {
+    return {
+      notFound: true
+    };
+  }
 
   return {
     props: {
@@ -38,11 +64,11 @@ export async function getStaticProps({ params, preview = false }) {
       body: data.project.body.raw
     }
   };
-}
+};
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths<IPostParams> = async () => {
   return {
     paths: [{ params: { slug: "super-theme" } }],
     fallback: true
   };
-}
+};
