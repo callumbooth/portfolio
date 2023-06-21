12d97a85571bab4edc00c732b0966012200937e6
Callum Booth <callum-booth@live.co.uk>
2023-06-21 16:43:16 +0100

    add work page

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,8 +1,29 @@
-
 export default function Home() {
+    
+    //get list of pages
+    //sort list
+    // 
+
     return (
-        <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-            <h1>work</h1>
+        <main className='flex flex-col'>
+            <h1 className='text-2xl pb-7'>
+                My latest work, both personal and professional
+            </h1>
+            <div className='block text-lg pb-7 lg:flex lg:gap-2 w-1/2'>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                    et massa mi. Aliquam in hendrerit urna. Pellentesque sit
+                    amet sapien fringilla, mattis ligula consectetur, ultrices
+                    mauris.
+                </p>
+            </div>
+
+            <div className='flex flex-wrap'>
+                <div className='flex-initial w-1/2'>article 1</div>
+                <div className='flex-initial w-1/2'>article 2</div>
+                <div className='flex-initial w-1/2'>article 3</div>
+                <div className='flex-initial w-1/2'>article 4</div>
+            </div>
         </main>
     );
 }
